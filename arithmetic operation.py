63eecdff11fc02f907992fac8dbc0589653596f7
jananimdev-22 <jananimdev@gmail.com>
2025-09-13 19:45:54 +0530

    Add files via upload

---
+++ arithmetic operation.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("Addition=",add)
+print("Subtraction=",sub)
+print("Multiplication=",mul)
+print("Division=",div)
