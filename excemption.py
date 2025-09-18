0f9d1331a925cb656436ad1e7b2ffd984907819b
jananimdev-22 <jananimdev@gmail.com>
2025-09-18 17:43:28 +0530

    Update excemption.py

--- excemption.py
+++ excemption.py
@@ -1,10 +1,11 @@
 try:
     n=int(input("Enter a value"))
-    res+60/n
-except zerodivisionerror:
+    res=60/n
+except ZeroDivisionError:
     if n==0:
         print("You have given as the value ,It is division zero error")
 else:
     print(res)
 finally:
     print("Bye")
+
